89660fd97452667a1febd92d50e1ad14da7755ae
ffniners <codycusimano@yahoo.com>
2023-10-27 14:06:08 -0700

    Events

--- components/WishList.jsx
+++ components/WishList.jsx
@@ -1,4 +1,96 @@
 import React from "react";
+import { Box, Input, Button, Stack, useToast, Textarea, Badge } from "@chakra-ui/react";
+import useAuth from "../hooks/useAuth";
+import { addEvent, getEventsByUser, updateEvent } from "../api/wishlist"; // Assuming you'll have an updateEvent method in the API
+
+const Events = () => {
+    const [eventName, setEventName] = React.useState("");
+    const [eventDate, setEventDate] = React.useState("");
+    const [events, setEvents] = React.useState([]);
+    const toast = useToast();
+    const { isLoggedIn, user } = useAuth();
+
+    React.useEffect(() => {
+        if (isLoggedIn) {
+            const unsubscribe = getEventsByUser(user.uid, setEvents);
+            return () => unsubscribe();
+        }
+    }, [isLoggedIn, user]);
+
+    const handleEventAdd = async () => {
+        if (!isLoggedIn) {
+            return;
+        }
+        const eventItem = {
+            eventName,
+            eventDate,
+            userId: user.uid,
+        };
+        await addEvent(eventItem);
+
+        setEventName("");
+        setEventDate("");
+        toast({ title: "Event added successfully", status: "success" });
+    };
+
+    const handleFieldChange = async (id, field, value) => {
+        setEvents(prevEvents => prevEvents.map(event => event.id === id ? {...event, [field]: value} : event));
+        await updateEvent(id, { [field]: value }); // You'll need to implement this updateEvent method in the API.
+    };
+
+    return (
+        <Box w="40%" margin="0 auto" display="block" mt={5}>
+            <Stack direction="column">
+                <Input
+                    placeholder="Event Name"
+                    value={eventName}
+                    onChange={(e) => setEventName(e.target.value)}
+                />
+                <Input
+                    type="date"
+                    placeholder="Event Date"
+                    value={eventDate}
+                    onChange={(e) => setEventDate(e.target.value)}
+                />
+                <Button onClick={handleEventAdd}>
+                    Add Event
+                </Button>
+            </Stack>
+            <Box mt={5}>
+                {events.map(event => (
+                    <Box
+                        key={event.id}
+                        p={3}
+                        mt={4}
+                        boxShadow="2xl"
+                        shadow={"dark-lg"}
+                        transition="0.2s"
+                        _hover={{ boxShadow: "sm" }}
+                    >
+                        <Input
+                            value={event.eventName}
+                            onChange={(e) => handleFieldChange(event.id, "eventName", e.target.value)}
+                        />
+                        <Input
+                            type="date"
+                            value={event.eventDate}
+                            onChange={(e) => handleFieldChange(event.id, "eventDate", e.target.value)}
+                        />
+                    </Box>
+                ))}
+            </Box>
+        </Box>
+    );
+};
+
+export default Events;
+
+
+
+
+/*
+
+import React from "react";
 import { Box, Input, Button, Stack, useToast, Text } from "@chakra-ui/react";
 import useAuth from "../hooks/useAuth";
 import { addEvent, getEventsByUser } from "../api/wishlist"; // Update API imports
@@ -62,70 +154,4 @@ const Events = () => {
 
 export default Events;
 
-
-
-/*import React from "react";
-import { Box, Input, Button, Stack, useToast, Text } from "@chakra-ui/react";
-import useAuth from "../hooks/useAuth";
-import { addWishListItem, getWishlistItemsByUser } from "../api/wishlist";
-
-const WishList = () => {
-    const [itemName, setItemName] = React.useState("");
-    const [itemCost, setItemCost] = React.useState("");
-    const [wishlistItems, setWishlistItems] = React.useState([]);
-    const toast = useToast();
-    const { isLoggedIn, user } = useAuth();
-
-    React.useEffect(() => {
-        if (isLoggedIn) {
-            const unsubscribe = getWishlistItemsByUser(user.uid, setWishlistItems);
-            return () => unsubscribe(); 
-        }
-    }, [isLoggedIn, user]);
-
-    const handleWishListAdd = async () => {
-        if (!isLoggedIn) {
-            
-            return;
-        }
-        const wishListItem = {
-            itemName,
-            itemCost,
-            userId: user.uid,
-        };
-        await addWishListItem(wishListItem);
-       
-        setItemName("");
-        setItemCost("");
-        toast({ title: "Item added to wishlist successfully", status: "success" });
-    };
-
-    return (
-        <Box w="40%" margin="0 auto" display="block" mt={5}>
-            <Stack direction="column">
-                <Input
-                    placeholder="Item Name"
-                    value={itemName}
-                    onChange={(e) => setItemName(e.target.value)}
-                />
-                <Input
-                    placeholder="Item Cost ($)"
-                    value={itemCost}
-                    onChange={(e) => setItemCost(e.target.value)}
-                />
-                <Button onClick={handleWishListAdd}>
-                    Add to Wishlist
-                </Button>
-            </Stack>
-            {wishlistItems.map(item => (
-                <Box key={item.id} mt={4}>
-                    <Text>{item.itemName} - ${item.itemCost}</Text>
-                    
-                </Box>
-            ))}
-        </Box>
-    );
-};
-
-export default WishList;
 */
