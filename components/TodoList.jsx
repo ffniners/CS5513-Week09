510311fdcab2450f0e4d966d1bcaf6859a80a356
ffniners <codycusimano@yahoo.com>
2023-10-27 14:50:54 -0700

    refactor test

--- components/TodoList.jsx
+++ components/TodoList.jsx
@@ -1,3 +1,119 @@
+import React, { useEffect } from "react";
+import {
+    Badge,
+    Box,
+    SimpleGrid,
+    useToast,
+    Input,
+    Textarea
+} from "@chakra-ui/react";
+import useAuth from "../hooks/useAuth";
+import { FaToggleOff, FaToggleOn, FaTrash } from "react-icons/fa";
+import { fetchDataByUser, updateDocument, deleteDocument, addDocument } from "../utils/Utils";
+
+const TodoList = () => {
+    const [todos, setTodos] = React.useState([]);
+    const { user } = useAuth();
+    const toast = useToast();
+
+    useEffect(() => {
+        if (user) {
+            const unsubscribe = fetchDataByUser("todo", user.uid, setTodos);
+            return () => unsubscribe();
+        }
+    }, [user]);
+
+    const handleTodoAdd = async (data) => {
+        await addDocument("todo", { ...data, userId: user.uid });
+        toast({ title: "Todo added successfully", status: "success" });
+    };
+
+    const handleTodoDelete = async (id) => {
+        if (confirm("Are you sure you wanna delete this todo?")) {
+            await deleteDocument("todo", id);
+            toast({ title: "Todo deleted successfully", status: "success" });
+        }
+    };
+
+    const handleToggle = async (id, status) => {
+        const newStatus = status === "completed" ? "pending" : "completed";
+        await updateDocument("todo", id, { status: newStatus });
+        toast({
+            title: `Todo marked ${newStatus}`,
+            status: newStatus === "completed" ? "success" : "warning",
+        });
+    };
+
+    const handleFieldChange = (id, field, value) => {
+        setTodos(prevTodos => prevTodos.map(todo => todo.id === id ? { ...todo, [field]: value } : todo));
+        updateDocument("todo", id, { [field]: value });
+    };
+
+    return (
+        <Box mt={5}>
+            <SimpleGrid columns={{ base: 1, md: 3 }} spacing={8}>
+                {todos.map((todo) => (
+                    <Box
+                        key={todo.id}
+                        p={3}
+                        boxShadow="2xl"
+                        shadow={"dark-lg"}
+                        transition="0.2s"
+                        _hover={{ boxShadow: "sm" }}
+                    >
+                        <Input
+                            value={todo.title}
+                            onChange={(e) => handleFieldChange(todo.id, "title", e.target.value)}
+                        />
+                        <Textarea
+                            value={todo.description}
+                            onChange={(e) => handleFieldChange(todo.id, "description", e.target.value)}
+                        />
+                        <Badge
+                            color="red.500"
+                            bg="inherit"
+                            transition={"0.2s"}
+                            _hover={{
+                                bg: "inherit",
+                                transform: "scale(1.2)",
+                            }}
+                            float="right"
+                            size="xs"
+                            onClick={() => handleTodoDelete(todo.id)}
+                        >
+                            <FaTrash />
+                        </Badge>
+                        <Badge
+                            color={todo.status === "pending" ? "gray.500" : "green.500"}
+                            bg="inherit"
+                            transition={"0.2s"}
+                            _hover={{
+                                bg: "inherit",
+                                transform: "scale(1.2)",
+                            }}
+                            float="right"
+                            size="xs"
+                            onClick={() => handleToggle(todo.id, todo.status)}
+                        >
+                            {todo.status === "pending" ? <FaToggleOff /> : <FaToggleOn />}
+                        </Badge>
+                        <Badge
+                            float="right"
+                            opacity="0.8"
+                            bg={todo.status === "pending" ? "yellow.500" : "green.500"}
+                        >
+                            {todo.status}
+                        </Badge>
+                    </Box>
+                ))}
+            </SimpleGrid>
+        </Box>
+    );
+};
+
+export default TodoList;
+
+/*
 import {
     Badge,
     Box,
@@ -121,118 +237,6 @@ const TodoList = () => {
     );
 };
 
-export default TodoList;
-
-
-/*
-
-import {
-    Badge,
-    Box,
-    Heading,
-    SimpleGrid,
-    Text,
-    useToast,
-} from "@chakra-ui/react";
-import React, { useEffect } from "react";
-import useAuth from "../hooks/useAuth";
-import { collection, onSnapshot, query, where } from "firebase/firestore";
-import { db } from "../firebase";
-import { FaToggleOff, FaToggleOn, FaTrash } from "react-icons/fa";
-import { deleteTodo, toggleTodoStatus } from "../api/todo";
-const TodoList = () => {
-    const [todos, setTodos] = React.useState([]);
-    const { user } = useAuth();
-    const toast = useToast();
-    const refreshData = () => {
-        if (!user) {
-            setTodos([]);
-            return;
-        }
-        const q = query(collection(db, "todo"), where("user", "==", user.uid));
-        onSnapshot(q, (querySnapchot) => {
-            let ar = [];
-            querySnapchot.docs.forEach((doc) => {
-                ar.push({ id: doc.id, ...doc.data() });
-            });
-            setTodos(ar);
-        });
-    };
-    useEffect(() => {
-        refreshData();
-    }, [refreshData, user]);
-    const handleTodoDelete = async (id) => {
-        if (confirm("Are you sure you wanna delete this todo?")) {
-            deleteTodo(id);
-            toast({ title: "Todo deleted successfully", status: "success" });
-        }
-    };
-    const handleToggle = async (id, status) => {
-        const newStatus = status == "completed" ? "pending" : "completed";
-        await toggleTodoStatus({ docId: id, status: newStatus });
-        toast({
-            title: `Todo marked ${newStatus}`,
-            status: newStatus == "completed" ? "success" : "warning",
-        });
-    };
-    return (
-        <Box mt={5}>
-            <SimpleGrid columns={{ base: 1, md: 3 }} spacing={8}>
-                {todos &&
-                    todos.map((todo) => (
-                        <Box
-                            key={todo.id}
-                            p={3}
-                            boxShadow="2xl"
-                            shadow={"dark-lg"}
-                            transition="0.2s"
-                            _hover={{ boxShadow: "sm" }}
-                        >
-                            <Heading as="h3" fontSize={"xl"}>
-                                {todo.title}{" "}
-                                <Badge
-                                    color="red.500"
-                                    bg="inherit"
-                                    transition={"0.2s"}
-                                    _hover={{
-                                        bg: "inherit",
-                                        transform: "scale(1.2)",
-                                    }}
-                                    float="right"
-                                    size="xs"
-                                    onClick={() => handleTodoDelete(todo.id)}
-                                >
-                                    <FaTrash />
-                                </Badge>
-                                <Badge
-                                    color={todo.status == "pending" ? "gray.500" : "green.500"}
-                                    bg="inherit"
-                                    transition={"0.2s"}
-                                    _hover={{
-                                        bg: "inherit",
-                                        transform: "scale(1.2)",
-                                    }}
-                                    float="right"
-                                    size="xs"
-                                    onClick={() => handleToggle(todo.id, todo.status)}
-                                >
-                                    {todo.status == "pending" ? <FaToggleOff /> : <FaToggleOn />}
-                                </Badge>
-                                <Badge
-                                    float="right"
-                                    opacity="0.8"
-                                    bg={todo.status == "pending" ? "yellow.500" : "green.500"}
-                                >
-                                    {todo.status}
-                                </Badge>
-                            </Heading>
-                            <Text>{todo.description}</Text>
-                        </Box>
-                    ))}
-            </SimpleGrid>
-        </Box>
-    );
-};
 export default TodoList;
 
 */
---
+++ utils/Utils.js
@@ -0,0 +1,29 @@
+import { collection, query, where, onSnapshot, updateDoc, doc, deleteDoc, addDoc } from "firebase/firestore";
+import { db } from "../firebase";
+
+export const fetchDataByUser = (collectionName, userId, setData) => {
+    const q = query(collection(db, collectionName), where("userId", "==", userId));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        let data = [];
+        querySnapshot.docs.forEach((doc) => {
+            data.push({ id: doc.id, ...doc.data() });
+        });
+        setData(data);
+    });
+    return unsubscribe; // Useful for cleanup in useEffect
+};
+
+export const updateDocument = (collectionName, docId, data) => {
+    const docRef = doc(db, collectionName, docId);
+    return updateDoc(docRef, data);
+};
+
+export const deleteDocument = (collectionName, docId) => {
+    const docRef = doc(db, collectionName, docId);
+    return deleteDoc(docRef);
+};
+
+export const addDocument = (collectionName, data) => {
+    const collRef = collection(db, collectionName);
+    return addDoc(collRef, data);
+};
